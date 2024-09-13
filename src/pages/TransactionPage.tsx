e4ab75fd31154fcaaf3cee1216a8e06a3bf53b06
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-13 16:25:15 +0500

    Moved Finance Frontend

---
+++ src/pages/TransactionPage.tsx
@@ -0,0 +1,10 @@
+import Transactions from "@/components/transactions/Transactions";
+
+const TransactionPage = () => {
+	return (
+		<div>
+			<Transactions />
+		</div>
+	);
+};
+export default TransactionPage;
---
+++ src/services/authService.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+const baseUrl =
+	"https://personal-finance-tracker-production.up.railway.app/api/v1/auth";
+
+type LoginUser = {
+	email: string;
+	password: string;
+};
+
+type RegisterUser = {
+	email: string;
+	username: string;
+	password: string;
+};
+
+interface LoginResponse {
+	token: string;
+	username: string;
+}
+
+interface RegisterResponse {
+	message: string;
+}
+
+const login = async (loginCredentials: LoginUser): Promise<LoginResponse> => {
+	const response = await axios.post(`${baseUrl}/login`, loginCredentials);
+	return response.data;
+};
+
+const register = async (
+	registerUserCredentials: RegisterUser
+): Promise<RegisterResponse> => {
+	const response = await axios.post(
+		`${baseUrl}/register`,
+		registerUserCredentials
+	);
+	return response.data;
+};
+
+export default {
+	login,
+	register,
+};
