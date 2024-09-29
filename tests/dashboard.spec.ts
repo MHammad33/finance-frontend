3f479830fb13b2b78a8bf05ea50c1d731a52dad9
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-29 17:53:13 +0500

    Add basic tests for Dashboard
    tests/dashboard.spec.ts
    
    Playwright tests

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -5,7 +5,28 @@ test.describe("Dashboard", () => {
     await page.goto("/");
   });
 
-  test("demo test", () => {
-    expect("1").toBe("1");
+  test("should display dashboard overview header", async ({ page }) => {
+    const header = page.locator("h1.text-3xl");
+    await expect(header).toHaveText("Dashboard Overview");
+  });
+
+  test("should display recent transactions section", async ({ page }) => {
+    const recentTransactions = await page.locator(
+      'section:has-text("Recent Transactions")'
+    );
+    await expect(recentTransactions).toBeVisible();
+  });
+
+  test("should display income and expenses charts", async ({ page }) => {
+    const incomeChart = page.locator("text=Income Chart");
+    const expensesChart = page.locator("text=Expenses Chart");
+
+    await expect(incomeChart).toBeVisible();
+    await expect(expensesChart).toBeVisible();
+  });
+
+  test("should display a budget section", async ({ page }) => {
+    const budgetSection = page.locator("text=Budget Section");
+    await expect(budgetSection).toBeVisible();
   });
 });
